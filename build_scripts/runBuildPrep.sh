c0213819102b440a4b921727e83d9fcf40dec15b
Eli Kamin <eli.kamin@hpe.com>
2022-05-25 14:02:26 -0500

    CRAYSAT-1387: Update builds for open source
    * Change internal pip repository references (arti.dev.cray.com) to
      reference external pip repository (artifactory.algol60.net)
    * Add Makefile to build Python package
    * Add runBuildPrepVenv and runUnitTestVenv.sh scripts.
    * Pin major version of PyYaml to < 6.0 to address test failures
    
    (cherry picked from commit b186dac20cc84093ddc576da676ac06ac801e6e7)

--- build_scripts/runBuildPrep.sh
+++ build_scripts/runBuildPrep.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-# (C) Copyright 2021-2022 Hewlett Packard Enterprise Development LP.
+#
+# MIT License
+#
+# (C) Copyright 2021-2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 
 # Set PIP_EXTRA_INDEX_URL to pull internal packages from internal locations:
-# arti.dev.cray.com/artifactory/csm-python-modules-remote/simple/ - repo containing cray-product-catalog
+# artifactory.algol60.net/artifactory/csm-python-modules/simple/ - repo containing cray-product-catalog
 # arti.dev.cray.com/artifactory/internal-pip-stable-local/ - repo containing nexusctl
-PIP_EXTRA_INDEX_URL="https://arti.dev.cray.com/artifactory/csm-python-modules-remote/simple/
+PIP_EXTRA_INDEX_URL="https://artifactory.algol60.net/artifactory/csm-python-modules/simple/
   https://arti.dev.cray.com/artifactory/internal-pip-stable-local/" pip3 install -r requirements.txt
 pip3 install -r requirements-dev.txt
